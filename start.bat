bd5b479f5c722eb326f5b9945948b31f19569b09
aufarakha <79144483+aufarakha@users.noreply.github.com>
2022-10-09 20:29:59 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -3,19 +3,19 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Server 2019 By MBAH GADGET" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user nabila MrZxRx107814 /add >nul
-net localgroup administrators nabila /add >nul
-net user nabila /active:yes >nul
+net user MrZRX MrZRx1078148 /add >nul
+net localgroup administrators MrZRX /add >nul
+net user MrZRX /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant nabila:F >nul
-ICACLS C:\Windows\installer /grant nabila:F >nul
+ICACLS C:\Windows\Temp /grant MrZRX:F >nul
+ICACLS C:\Windows\installer /grant MrZRX:F >nul
 echo Success!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
-echo Username: nabila
-echo Password: MrZxRx107814
+echo Username: MrZRX
+echo Password: MrZRx1078148
 echo You can login now.
 ping -n 10 127.0.0.1 >null
